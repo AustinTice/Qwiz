046533e6f4343afbb1c381949823fa1aedbfd3c1
Austin Tice <austin.tice4@gmail.com>
2018-07-05 23:56:07 -0500

    Added code documentation

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ let randomNumber2 = Math.floor(Math.random() * 20);
 button.addEventListener("click", checkAnswer);
 let signPicked = "";
 
+// Checking to see if the window has loaded and once it has we will grab the first question.
 window.addEventListener("load", function(){
   getRandomSign();
   postRandomNumbersForQuestion();
 })
 
+// This function is just a simple and easy-to-read definition to help post the next question to the screen.
 function postRandomNumbersForQuestion(){
   question.innerHTML = `What is ${randomNumber1} ${signPicked} ${randomNumber2}?`
 }
 
+// This function will run once the submit button is clicked and allow for input validation, and
+// to check the validity of the answer given.
 function checkAnswer() {
   let correctAnswer = eval(`randomNumber1 ${signPicked} randomNumber2`);
   if(answer.value == correctAnswer){
@@ -26,17 +30,21 @@ function checkAnswer() {
   reset();
 }
 
+// Interacts with the reset function in allowing a new random sign to be chosen
+// for each question.
 function getRandomSign(){
   let possibleSigns = ["+", "-", "*", "/" ];
   let randomIndex = Math.floor(Math.random() * (possibleSigns.length - 1));
   signPicked = possibleSigns[randomIndex];
 }
 
+// Allows for new random numbers to be chosen for each question
 function shuffleRandomNumber(){
   randomNumber1 = Math.floor(Math.random() * 20);
   randomNumber2 = Math.floor(Math.random() * 20);
 }
 
+// This function contains the code that resets the problems after each and every answer is submitted.
 function reset(){
   answer.value = "";
   shuffleRandomNumber();
