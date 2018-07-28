3ee15c2e0d3ef28ca97f9e9db23d53dd376c2d9c
Austin Tice <austin.tice4@gmail.com>
2018-07-27 23:28:49 -0500

    Adding stylistic things along with documentation

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ function getRandomSign(){
 }
 
 // Allows for new random numbers to be chosen for each question
+// by shuffling each of the numbers that will be presented
 function shuffleRandomNumber(){
   randomNumber1 = Math.floor(Math.random() * 20);
   randomNumber2 = Math.floor(Math.random() * 20);
